10d58c0fd6f0b665161b03bdea4c59ac34fbfc59
Alison Feldish <alison.feldish@gmail.com>
2020-12-07 19:01:38 -0500

    api test

--- src/Day/index.js
+++ src/Day/index.js
@@ -4,7 +4,24 @@ const Day = (props) => {
 
     return (
         <div className="wrapper">
-            <div>test test test</div>
+            <div className="today">
+                <div className="row1">
+                    <img className="today-icon" src=""/>
+                    <div className="column2">
+                        <span className="current-city">test test test</span>
+                        <span className="today-date"></span>
+                    </div>
+                </div>
+                {/* <div className="row2">
+                    <span className="today-range"></span>
+                    <span className="today-temp"></span>
+                    <span className="today-feel"></span>
+                </div>
+                <div className="row3">
+                    <span className="today-description"></span>
+                    <span className="next-days" onClick={() => setNextDays(!nextDays)}><img className="arrow" src={imgsrc} /></span>
+                </div> */}
+            </div>
         </div>
     )
 }
